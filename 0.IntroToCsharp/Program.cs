baffd4c3b1554e8f17453d9c328fba876eb45cb4
Taurean Roberts <taureanjroberts@gmail.com>
2018-04-10 18:01:55 -0500

    Initial

---
+++ 0.IntroToCsharp/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0.IntroToCsharp
+{
+    public abstract class Item //Abstract
+    {
+        protected string name;
+        protected int Id;
+    }
+    public abstract class Weapon : Item //Abstract
+    {
+        public int power;
+        public virtual void DoDamage(int amount)
+        {
+            
+        }
+    }
+    public abstract class Potion : Item //Abstract
+    {
+        protected int Power;
+        protected virtual void Consume()
+        {
+
+        } 
+    }
+    public class Elixir : Potion //Concrete
+    {
+
+    }
+    public class Ether : Potion //Concrete
+    {
+
+    }
+    public class BusterSword : Weapon //Concrete
+    {
+        public void Swing()
+        {
+
+        }
+    }
+    public class GatlingGun : Weapon //Concrete
+    {
+        public void Shoot()
+        {
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ 1.Containers/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+
+namespace _1.Containers
+{
+    public class Stat
+    {
+        public string Name;
+        public int Value;
+        public string Description;
+    }
+
+    public class SavingThrows
+    {
+        public int Strength;
+        public int Dexterity;
+        public int Constitution;
+        public int Intelligence;
+        public int Charisma;
+    }
+
+    public class Character
+    {
+        Dictionary<string, Stat> SavingThows = new Dictionary<string, Stat>();
+
+        public int ExperiencePoints;
+        public string CharacterName;
+        public int level;
+
+    }
+    class Program
+    {
+        //Make a dictionary of <string, stat>
+        static void Main(string[] args)
+        {
+            Dictionary<string, Stat> Stats = new Dictionary<string, Stat>();
+            Stats.Add("Strength", new Stat());
+            Stats.Add("Dexterity", new Stat());
+            Stats.Add("Constitution", new Stat());
+            Stats.Add("Intelligence", new Stat());
+            Stats.Add("Wisdom", new Stat());
+            Stats.Add("Charisma", new Stat());
+            //Using the string as a key store the specified stat
+        }
+    }
+}
