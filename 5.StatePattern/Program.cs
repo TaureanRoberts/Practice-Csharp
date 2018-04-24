6ba466e2aaf4c88393f27ccd42bb6a3219d63f4e
Taurean Roberts <taureanjroberts@gmail.com>
2018-04-24 17:08:28 -0500

    Diving into the code.

--- 5.StatePattern/Program.cs
+++ 5.StatePattern/Program.cs
@@ -1,22 +1,117 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Dynamic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
 
 namespace _5.StatePattern
 {
-    static class Program
+    public interface IContext
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
+        IState CurrentState { get; set; }
+        void UpdateContent();
+        void ChangeState();
+    }
+
+    public interface IState
+    {
+        void OnEnter(IContext other);
+        void UpdateState(IContext other);
+        void OnExit(IContext other);
+    }
+
+    public class GameStartState : IState, IContext
+    {
+        public void OnEnter(IContext other)
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+
+        }
+
+        public void UpdateState(IContext other)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnExit(IContext other)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IState CurrentState { get; set; }
+        public void UpdateContent()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ChangeState()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class GameRunningState : IState, IContext
+    {
+        public void OnEnter(IContext other)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateState(IContext other)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnExit(IContext other)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IState CurrentState { get; set; }
+        public void UpdateContent()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ChangeState()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class GameExitState : IState, IContext
+    {
+        public void OnEnter(IContext other)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateState(IContext other)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnExit(IContext other)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IState CurrentState { get; set; }
+        public void UpdateContent()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ChangeState()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
         }
     }
 }
--- 5.StatePattern/Properties/AssemblyInfo.cs
+++ 5.StatePattern/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("9c8e468e-2b00-4686-9b5a-afca8afd0699")]
+[assembly: Guid("8b93466d-4542-43cf-9876-d268a981c664")]
 
 // Version information for an assembly consists of the following four values:
 //
